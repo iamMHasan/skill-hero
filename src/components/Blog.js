f947c609e4f892958d31eb6135288674a3527685
iamMHasan <abcimhasanbro@gmail.com>
2022-10-25 19:22:26 +0600

    blog added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,9 +2,32 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            blog
+        <section className="dark:bg-gray-800 dark:text-gray-100 h-[100vh]">
+        <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+            <h2 className="text-2xl font-semibold sm:text-4xl text-center py-3">Some important questions</h2>
+            <div className="space-y-4">
+                <details className="w-full border rounded-lg">
+                    <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">what is cors?</summary>
+                    <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources </p>
+                </details>
+                <details className="w-full border rounded-lg">
+                    <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Why are you using firebase? What other options do you have to implement authentication?</summary>
+                    <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together. <br />
+                    Auth0, MongoDB, Passport, Okta, and Firebase are the most popular alternatives and competitors to Firebase Authentication. </p>
+                </details>
+                <details className="w-full border rounded-lg">
+                    <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">How does the private route work?
+                    </summary>
+                    <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">The react private route component renders child components ( children ) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property. </p>
+                </details>
+                <details className="w-full border rounded-lg">
+                    <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">What is Node? How does Node work?
+                    </summary>
+                    <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive. </p>
+                </details>
+            </div>
         </div>
+    </section>
     );
 };
 
