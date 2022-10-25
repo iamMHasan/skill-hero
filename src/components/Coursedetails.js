73730a3ca731f9422eda2268a8d9e177d2090276
iamMHasan <abcimhasanbro@gmail.com>
2022-10-25 18:19:36 +0600

    fixed courses page

--- src/components/Coursedetails.js
+++ src/components/Coursedetails.js
@@ -6,18 +6,16 @@ const Coursedetails = () => {
     console.log(courseDetails);
     const {image, name} = courseDetails
     return (
-        <div>
-            <div className="p-5 mx-auto sm:p-10 md:p-16  text-gray-100">
+          <div className="p-5 mx-auto sm:p-10 md:p-16  text-gray-100">
                 <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
                     <img src={image} alt="" className="w-full h-60 sm:h-96 bg-gray-500" />
-                    <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-yellow-500">
+                    <div className="p-6 pb-12 m-4 mx-auto -mt-12 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-yellow-500">
                         <div className="space-y-2 h-24 flex items-center justify-center">
                             <h1 >{name}</h1> 
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
     );
 };
 
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import { useLoaderData , Link} from 'react-router-dom'
+import { useLoaderData, Link } from 'react-router-dom'
 const Courses = () => {
 
     const courses = useLoaderData()
     console.log(courses);
 
     return (
-        <div className='grid grid-cols-3 gap-3 my-4 p-5 mx-auto w-[1200px] '>
-            {
-                courses.map(course => (
-                    <div className="max-w-xs rounded-md shadow-md bg-gray-900 text-gray-100 p-1">
-                        <img src={course.image} alt="" className="object-cover object-center w-full rounded-t-md h-72" />
-                        <div className="flex flex-col justify-between p-6 space-y-8">
-                            <div className="space-y-2">
-                                <h2 className="text-3xl font-semibold tracking-wide">{course.name}</h2>
-                                <p className="text-gray-100">Enroll to Build your future now!</p>
+        <div className='grid md:grid-cols-4 grid-cols-1 my-4 w-[1240px] mx-auto'>
+            <div>
+                {
+                    courses.map(course => (
+                        <Link to={`/courses/${course.id}`}>
+                            <p className='flex flex-col p-2 hover:text-white cursor-pointer font-bold'>{course.name}</p>
+                        </Link>
+                    ))
+                }
+            </div>
+            <div className='col-span-3'>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-1 md:gap-3 '>
+                    {
+                        courses.map(course => (
+                            <div className="max-w-xs rounded-md shadow-md bg-gray-900 text-gray-100 p-1">
+                                <img src={course.image} alt="" className="object-cover object-center w-full rounded-t-md h-[200px]  md:h-72" />
+                                <div className="flex flex-col justify-between p-6 space-y-8">
+                                    <div className="space-y-2">
+                                        <h2 className="text-3xl font-semibold tracking-wide">{course.name}</h2>
+                                        <p className="text-gray-100">Enroll to Build your future now!</p>
+                                    </div>
+                                    <Link to={`/courses/${course.id}`}>
+                                        <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-violet-400 text-gray-900">Enroll Now</button>
+                                    </Link>
+                                </div>
                             </div>
-                            <Link to={`/courses/${course.id}`}>
-                            <button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-violet-400 text-gray-900">Enroll Now</button>
-                            </Link>
-                        </div>
-                    </div>
-                ))
-            }
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     );
 };
