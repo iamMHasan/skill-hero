976a3f20a10e4151b1f033623c76c6e316d85037
iamMHasan <abcimhasanbro@gmail.com>
2022-10-25 10:20:47 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter , RouterProvider} from 'react-router-dom';
+import Home from './components/Home';
+import Main from './layout/Main';
+import Courses from './components/Courses';
+import Blog from './components/Blog';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path : '/',
+      element : <Main></Main>,
+      children : [
+        {
+          path : '/',
+          element : <Home></Home>
+        },
+        {
+          path : '/courses',
+          loader : () => fetch('http://localhost:5000/courses'),
+          element : <Courses></Courses>,
+        },
+        {
+          path : '/blog',
+          element : <Blog></Blog>
+        }
+      ]
+    },
+   
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Courses.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useLoaderData} from 'react-router-dom'
+const Courses = () => {
+
+    const courses = useLoaderData()
+    console.log(courses);
+
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default Courses;
